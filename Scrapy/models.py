e684e8b4c67878048173baa279cdfdc6de419e45
Hsekaj B <jakesh.bohaju@gmail.com>
2019-03-28 07:05:29 +0545

    Final version

--- Scrapy/models.py
+++ Scrapy/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 
-class TheodoTeam(models.Model):
-    name = models.CharField(max_length=150)
-    image = models.CharField(max_length=150)
-    fun_fact = models.TextField(blank=True)
-
-    class Meta:
-        verbose_name = "theodo UK team"
-
-
 class NewsScrapy(models.Model):
     title = models.CharField(max_length=200)
     news = models.TextField(blank=True)
--- Scrapy/views.py
+++ Scrapy/views.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from django.views import View
 
 from Scrapy.models import NewsScrapy
-import re
+from datetime import date
 
 
 class Index(View):
@@ -19,11 +19,21 @@ class Index(View):
         a_tags = homepage_parsed.find_all('a')
         links = set()
 
+        # get present os date and convert into yyyy-mm-dd format
+        today = str(date.today())
+        year = today[0:4]
+        month = today[5:7]
+        day = today[8:10]
+        print(year, month, day)
+        present_date = year + '-' + month + '-' + day
+        print(present_date)
+
+
         # a_tags is list of dictionary form
         for a_tag in a_tags:
             # print(a_tag.get('href'))
             # link = a_tag.get('href', '')
-            if '/2019/03/24/' in a_tag.get('href', ''):
+            if '/' + year + '/' + month + '/' + day + '/' in a_tag.get('href', ''):
                 # print("link success")
                 links.add(a_tag.get('href'))
                 # links.add(link)
@@ -36,29 +46,32 @@ class Index(View):
 
             title = single_news_parsed.find('h1').text
             print(title)
-            nc = single_news_parsed.find('div', {'class': 'entry-content'})
-            ppp = nc.find_all('p')
-            image = nc.find_all('img')
-            a = []
-            for innn in image:
-                a.append(innn['src'])
-            a = a[1:2]
-            # print(a)
-            a = '\n'.join(a)
-            # print(a)
+            if not Blog.title == title and Blog.blogdate == present_date:
+                nc = single_news_parsed.find('div', {'class': 'entry-content'})
+                ppp = nc.find_all('p')
+                image = nc.find_all('img')
+                a = []
+                for innn in image:
+                    a.append(innn['src'])
+                a = a[1:2]
+                # print(a)
+                a = '\n'.join(a)
+                # print(a)
 
-            lists = []
-            for i in ppp:
-                news = i.text
-                print(news)
-                lists.append(news)
-            lists = '\n'.join(lists)
-            print(lists)
-            newsscrapy.title = title
-            newsscrapy.image = a
-            newsscrapy.news = lists
-            newsscrapy.save()
-            print("=========================New post============================================================")
+                lists = []
+                for i in ppp:
+                    news = i.text
+                    # print(news)
+                    lists.append(news)
+                lists = '\n'.join(lists)
+                # print(lists)
+
+                newsscrapy.title = title
+                newsscrapy.image = a
+                newsscrapy.news = lists
+                newsscrapy.save()
+                print("=========================New post============================================================")
+
         template_content = {
             'scraps': NewsScrapy.objects.all()
         }
